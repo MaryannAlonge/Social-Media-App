67dd441f2a51d8a8a02cf48dcbac321ae43e0d99
Jumoke Alonge <ariyikemaryann@gmail.com>
2022-12-28 13:26:58 +0100

    editing

--- index.js
+++ index.js
@@ -47,12 +47,14 @@ menuItems.forEach(item => {
 })
 
 
+
+
 // ======================MESSAGES=================== ---->
 // searches chat
 const searchMessage = () => {
   const val = messageSearch.value.toLowerCase();
   message.forEach(user => {
-    let name = user.querySelector('h5').textContent.toLowerCase();
+    let name = user.querySelectorAll('h5').textContent.toLowerCase();
     if (name.indexOf(val) != -1) {
       user.style.display = 'flex';
     } else {
